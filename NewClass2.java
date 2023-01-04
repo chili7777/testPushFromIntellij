15272596df3ddda4cf06c723e770f3f80296e257
Hanmilton Berrezueta <hberrezu@pichincha.com>
2023-01-04 16:48:13 -0500

    Here is the test coommit from intellj

---
+++ NewClass2.java
@@ -0,0 +1,20 @@
+public final class NewClass2 {
+
+    private int num1;
+
+    private int num2;
+
+    private int num3;
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public int getNum3() {
+        return num3;
+    }
+}
---
+++ NewClassTest.java
@@ -0,0 +1,6 @@
+public final class NewClassTest {
+
+    public static void main(String[] args) {
+        System.out.println("this is a test for intellj push to git");
+    }
+}
